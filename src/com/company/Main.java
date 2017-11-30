cfa5b40a0767267bafa4a8e27cc0761ef670338a
szililaszlo <laszlo0919@gmail.com>
2017-11-30 22:27:16 +0100

    #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,5 +20,26 @@ public class Main {
         //kiíratás
         System.out.println("A téglalap területe: " + area + " cm^2");
         System.out.println("A téglalap kerülete: " + kerulet+ " cm");
+        System.out.println("------------------------------");
+
+        //2.
+        //Bekérem a deciliter mennyiségét
+        System.out.println("Kérem a deciliter mennyiségét: ");
+        Scanner amount = new Scanner(System.in);
+        //elmentem a mennyiséget változóba
+        int deciliter = amount.nextInt();
+        //elvégzem az átváltásokat, double -re átkasztolom, hogy megfelelő legyen aaz osztás eredménye
+        double liter = (double)deciliter/10;
+        double hektoliter = (double)deciliter/100;
+        int centiliter = deciliter*10;
+        int mililiter = deciliter*100;
+        //kiíratás
+        System.out.println(deciliter + " dl = " + mililiter + " ml");
+        System.out.println(deciliter + " dl = " + centiliter + " cl");
+        System.out.println(deciliter + " dl = " + liter+ " l");
+        System.out.println(deciliter + " dl = " + hektoliter + " hl");
+        System.out.println("------------------------------");
+
+
     }
 }
